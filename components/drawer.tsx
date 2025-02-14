12c77806cb2b7e40da282b4f407dfeee4053f04e
Vicente Rivera <vrivera.dev@gmail.com>
2025-02-14 16:28:04 -0300

    drawer base

--- components/drawer.tsx
+++ components/drawer.tsx
@@ -6,16 +6,21 @@ import {
 import { ThemedText } from "@/components/ui/ThemedText";
 import { ThemedView } from "@/components/ui/ThemedView";
 import { View } from "react-native";
+import { APP_NAME } from "@/constants/brand";
+import { Link } from "expo-router";
 
 const CustomDrawer = (props: DrawerContentComponentProps) => {
   return (
-    <DrawerContentScrollView {...props} scrollEnabled={false}>
-      {/* <ThemedView className="flex flex-col gap-y-4">
-        <ThemedText type="title">Ponderador</ThemedText>
-      </ThemedView> */}
+    <DrawerContentScrollView {...props} scrollEnabled={false} contentContainerStyle={{flex: 1}}>
+      <ThemedView className="flex flex-col gap-y-4 justify-between h-full">
+        <View>
+          <ThemedText type="title">{APP_NAME}</ThemedText>
+        </View>
+        <ThemedText>
+          Desarrollado por <Link className="underline italic" href="https://vrivera.is-a.dev">Vicente Rivera.</Link>
+        </ThemedText>
+      </ThemedView>
       {/* <DrawerItemList {...props} /> */}
-      {/* <View className="my-4 bg-black dark:bg-white/80 h-px w-full" /> */}
-      <ThemedText>coming soon..</ThemedText>
     </DrawerContentScrollView>
   );
 };
